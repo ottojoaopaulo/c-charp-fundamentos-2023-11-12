71ba6d4a6514cdf8097e598e76429c7eab0007f8
Joao Paulo Otto <74639@PROWAY.TREINA>
2023-11-26 18:23:33 -0300

    Finalizado exemplo de leitura e escrita manual em arquivos .csv

--- CSharp09Arquivo/Program.cs
+++ CSharp09Arquivo/Program.cs
@@ -1,4 +1,6 @@
 ﻿
+using ConsoleTables;
+
 string caminho = "C:\\Users\\74639\\Desktop\\arquivo.csv";
 
 if (File.Exists(caminho) == false)
@@ -6,14 +8,45 @@ if (File.Exists(caminho) == false)
     string cabecalho = "Nome;Idade\n";
     File.WriteAllText(caminho, cabecalho);
 }
-//else
-//{
-//    string registrosTexto = File.ReadAllText(caminho);
-//    string[] registros = registrosTexto.Split("\n");
-//}
+else
+{
+    string[] registros = File.ReadAllLines(caminho);
+
+    int quantidadeRegistros = registros.Length;
+
+    Console.WriteLine("Registros do arquivo .csv");
+
+    ConsoleTable tabela = new ConsoleTable ("nome" , "idade");
 
 
-Console.Write("Deseja cadastrar algum registro: [S/N]");
+    int indice = 0;
+
+    while (indice < quantidadeRegistros)
+    {
+        if (indice == 0)
+        {
+            indice = indice + 1;
+            continue;
+        }
+
+
+        string registro = registros[indice];
+
+        string[] colunas = registro.Split(';');
+
+        string nome = colunas[0];
+
+        int idade = Convert.ToInt32(colunas[1]);
+        tabela.AddRow(nome, idade);
+
+        indice = indice + 1;
+    }
+
+    tabela.Write();
+}
+
+
+Console.Write("\nDeseja cadastrar algum registro: [S/N]");
 string desejaContinuar = Console.ReadLine().Trim().ToLower();
 
 while(desejaContinuar == "S")
