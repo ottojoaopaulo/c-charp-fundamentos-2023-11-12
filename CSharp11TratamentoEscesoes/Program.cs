8a129d1eb893e98a219002614c6acff0c589c813
Joao Paulo Otto <74639@PROWAY.TREINA>
2023-11-26 18:54:28 -0300

    Exemplo de tratamento de exceçoes

---
+++ CSharp11TratamentoEscesoes/Program.cs
@@ -0,0 +1,44 @@
+﻿//try
+//{
+//    int numero1 = Convert.ToInt32("10");
+//    int numero2 = Convert.ToInt32("12p");
+
+//    int some = numero1 + numero2;   
+
+//    Console.WriteLine("Numero: "+ numero1);
+//    Console.WriteLine("Numero: " + numero2);
+//}
+//catch(Exception e)
+//{
+//    Console.WriteLine("Aconteceu um erro na conversãodo numero");
+//}
+
+//Console.WriteLine("Programa encerrou corretamente");
+
+Console.Write("Digite o nome");
+string nome = Console.ReadLine();
+int idade = 0;
+
+try
+{
+    Console.WriteLine("Digite sua idade");
+    idade = Convert .ToInt32(Console.ReadLine());
+}
+catch (Exception e)
+{
+    Console.WriteLine("Idade não e um numero inteiro valido");
+}
+
+double peso = 0;
+
+try
+{
+    Console.WriteLine("Digite o seu peso");
+    peso = Convert.ToDouble(Console.ReadLine());
+}
+catch (Exception e)
+{
+    Console.WriteLine("Peso não e um valor real valido");
+}
+
+Console.WriteLine("Nome " + nome "\nIdade" + idade "\npeso" + peso);
