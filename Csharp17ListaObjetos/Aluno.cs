2806769a96a25daf7ead21b4fcd221d64b9d044f
Joao Paulo Otto <74639@PROWAY.TREINA>
2023-12-17 17:31:30 -0300

    Incluindo lista de objetos

---
+++ Csharp17ListaObjetos/Aluno.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp17ListaObjetos
+{
+    internal class Aluno
+    {
+        public string Nome { get; set; }
+        public double Nota1 { get; set; }
+        public double Nota2 { get; set; }
+        public double Nota3 { get; set; }
+
+    }
+
+
+}
